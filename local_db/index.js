9f705f1e1ac2f7f27c6383d37c1d9f126dc5a824
Walze <catufuzgu@gmail.com>
2017-11-22 16:21:49 -0200

    localDB added

---
+++ local_db/index.js
@@ -0,0 +1,31 @@
+console.log('Started!')
+
+var PouchDB = require('pouchdb');
+var db = new PouchDB('my_database');
+
+
+var db = new PouchDB('todos');
+var remoteCouch = false;
+
+function addTodo(text) {
+  var todo = {
+    _id: new Date().toISOString(),
+    title: text,
+    completed: false
+  };
+  db.put(todo, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a todo!');
+    }
+  });
+}
+
+function getAll() { 
+  db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
+    console.log(doc.rows);
+  });
+}
+
+addTodo('lol');
+
+getAll();
