4645f5ef5359b648fac414e54c154a02501b9dc5
Walze <catufuzgu@gmail.com>
2017-11-22 16:36:10 -0200

    dupes removed index modified

--- local_db/index.js
+++ local_db/index.js
@@ -1,18 +1,14 @@
 console.log('Started!')
 
 var PouchDB = require('pouchdb');
-var db = new PouchDB('my_database');
-
-
 var db = new PouchDB('todos');
 
 function addTodo(text) {
   var todo = {
     _id: new Date().toISOString(),
     title: text,
-    completed: false
   };
-  db.put(todo, function callback(err, result) {
+  db.put(todo, (err, result) => {
     if (!err) {
       console.log('Successfully posted a todo!');
     }
@@ -20,11 +16,11 @@ function addTodo(text) {
 }
 
 function getAll() {
-  db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
+  db.allDocs({ descending: true }).then((doc) => {
     console.log(doc.rows);
   });
 }
 
-addTodo('lol');
+//addTodo('lol');
 
 getAll();
