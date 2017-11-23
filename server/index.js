e4f311a8a31cdce04dcb75af27a137387b3dcaa6
Walze <catufuzgu@gmail.com>
2017-11-23 16:15:53 -0200

    removed database

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 const express = require('express')
-const DataBase = require('./db');
+const DataBase = require('./db')
 const bodyParser = require('body-parser')
-const db = new DataBase;
-const app = express();
+const db = new DataBase
+const app = express()
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
-app.listen(3001, () => console.log('Listening on Port 3001'));
+app.listen(3001, () => console.log('Listening on Port 3001'))
 
 app.get('/', (req, res) => {
   db.allDocs().then((doc) => {
-    res.send(doc.rows);
+    res.send(doc.rows)
   })
 })
 
-app.all('/test', (req, res) => {
+app.all('/user', (req, res) => {
   res.send(req.body)
 })
